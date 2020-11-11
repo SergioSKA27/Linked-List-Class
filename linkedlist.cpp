6aa1a2a7428ca83858fad8e8dc602c51f4924f69
Sergio Lopez Martinez <70881259+SergioSKA27@users.noreply.github.com>
2020-11-11 04:41:12 -0600

    Add files via upload
    añadí funciones  append ,at, push , print.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -25,18 +25,18 @@ public:
     }
 
     Node<type> &changedata(type newData)
-    {
+    { //cambia los datos del nodo
         Data = newData;
         return *this;
     }
 
     void showdata()
-    {
+    { //muestra los datos del nodo
         std::cout << Data << std::endl;
     }
 
     type getdata()
-    {
+    { //retorna la data como valor
         return this->Data;
     }
 
@@ -70,7 +70,7 @@ public:
         }
     }
 
-    void Push(ty Data)
+    void push(ty Data)
     { // inserta un elemento al principio de la lista
 
         Node<ty> *tmp = HeadrefS;
@@ -84,7 +84,7 @@ public:
 
         tmp = nde;
 
-        if (size == 0)
+        if (size == 1)
             TailrefS = nde;
         else
         {
@@ -96,21 +96,83 @@ public:
         }
     }
 
-    Node<ty> *Head()
-    {
-        HeadrefS->showdata();
+    void append(ty Data)
+    { //inserta elementos al final de la lista
+        Node<ty> *tmp = HeadrefS;
+        nde = new Node<ty>();
+        nde->changedata(Data);
+
+        if (TailrefS == NULL)
+        {
+            nde->next = HeadrefS;
+            HeadrefS = nde;
+            TailrefS = nde;
+            size += 1;
+        }
+        else
+        {
+            while (tmp->next != NULL)
+            {
+                tmp = tmp->next;
+            }
+            tmp->next = nde;
+            TailrefS = tmp->next;
+            size += 1;
+        }
+    }
+
+    Node<ty> *head()
+    { // retorna un puntero a head
+
+        HeadrefS->showdata(); // eliminalo si quieres
         return this->HeadrefS;
     }
 
-    Node<ty> *Tail()
-    {
-        TailrefS->showdata();
+    Node<ty> *tail()
+    { //retorna un puntero a tail(final de la lista)
+
+        TailrefS->showdata(); // eliminalo si quieres
         return this->TailrefS;
     }
+
+    Node<ty> *at(int index)
+    { //retorna el elemto de la lista indicado
+        //los indices empiezan en 0
+
+        Node<ty> *tmp = nde;
+        int k = 0;
+
+        while (k < index && tmp != NULL)
+        {
+            tmp = tmp->next;
+            k++;
+        }
+        tmp->showdata(); // eliminalo si quieres
+        return tmp;
+    }
+
     int listsize()
-    {
+    { //retorna el tamaño de la lista
         return this->size;
     }
+
+    void printlist()
+    { // imprime la lista
+        Node<ty> *tmp = nde;
+        int k = 0;
+
+        while (tmp != NULL)
+        {
+            if (k == 0)
+                std::cout << tmp->getdata();
+            else
+                std::cout << "-> " << tmp->getdata();
+            tmp = tmp->next;
+            k++;
+        }
+        std::cout << std::endl;
+    }
+
     ~linkedlist()
     {
     }
@@ -118,15 +180,20 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    linkedlist<int> L('s');
+    linkedlist<double> L('s');
 
-    L.Push(150); //tail
-    L.Push(100);
-    L.Push(500);
-    L.Push(10000);
-    L.Push(120); //head
-    L.Head();
-    L.Tail();
+    L.push(150); //tail
+    L.push(100.56546);
+    L.push(500);
+    L.push(10000);
+    L.append(-5);
+    L.append(-2);
+    L.append(-1);
+    L.push(120); //head
+    L.head();
+    L.tail();
+    L.printlist();
+    L.at(L.listsize() - 1);
 
     return 0;
 }
